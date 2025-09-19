a5d0a9d2df38896a9ab546f69518d413f9b202df
Guillermo <guillermo.sotelo@volvocars.com>
2025-09-19 16:01:54 +0200

    mobile-rotation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/Layout/Layout'
 
 type Props = {}
 
 export default function Home({ }: Props) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => window.innerWidth <= 768
+    setIsMobile(checkMobile())
+    window.addEventListener("resize", () => setIsMobile(checkMobile()))
+    return () => window.removeEventListener("resize", () => setIsMobile(checkMobile()))
+  }, [])
+
   return (
-    <div className="home__container">
+    <div className={`home__container${isMobile ? '--rotated' : ''}`}>
       <Layout />
     </div>
   )
